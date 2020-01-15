6da83ef4a7154f0e4ac938e2f1b8506889ea1d98
HuaYJ1996 <huayjh@yonyou.com>
2020-01-15 09:55:37 +0800

    feat: 预览代码高亮显示

--- templates/templateHtml.js
+++ templates/templateHtml.js
@@ -5,7 +5,7 @@ module.exports = `<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Demo</title>
-    <link href="./hightlight/styles/vs2015.css" rel="stylesheet" />
+    <link href="./hightlight/styles/github.css" rel="stylesheet" />
     @cssImport
 </head>
 <body>
