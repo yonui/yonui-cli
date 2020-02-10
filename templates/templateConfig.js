e120181b0b95fdc21ca276e26ecc0d9b94f419f5
HuaYJ <huayjh.yonyou.com>
2020-02-10 14:31:10 +0800

    upd: 修改libra.config.json文件的写入方式

--- templates/templateConfig.js
+++ templates/templateConfig.js
@@ -1,12 +1,12 @@
-{
+module.exports = `{
     "port": "8090",
     "autoTemplate": true,
     "sourcePath": "./components",
-    "type": "ts",
+    "type": "@type",
     "lib": [
     ],
     "extraImport": {
         "js": "",
         "css": ""
     }
-}
+}`
