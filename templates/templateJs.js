acaeb413538c16dced4059997c160eaca2d426ec
HuaYJ1996 <huayjh@yonyou.com>
2020-02-10 10:27:34 +0800

    模版代码更新

--- templates/templateJs.js
+++ templates/templateJs.js
@@ -14,6 +14,7 @@ import marded from 'marked';
 import Highlight from 'react-highlight';
 import 'highlight.js/styles/github.css';
 import './index.less';
+import cb from '@mdf/cube/lib/cube';
 export default class IndexView extends Component {
     constructor(props) {
         super(props)
