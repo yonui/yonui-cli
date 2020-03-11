0c323d1f6777ebcafd08ea369057c67b43ad3277
HuaYJ1996 <1020451244@qq.com>
2020-03-11 11:15:26 +0800

    style: 文件名修改

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,12 +1,12 @@
 const init = require('./init')
-const buildProject = require('./build')
-const developProject = require('./start')
+const build = require('./build')
+const start = require('./start')
 const create = require('./create')
 const compress = require('./compress')
 module.exports = {
-  init: init,
-  build: buildProject,
-  start: developProject,
+  init,
+  build,
+  start,
   create,
   compress
 }
