a94c41794d8eeef3252363b9ad7b3a50574f7259
HuaYJ1996 <1020451244@qq.com>
2020-03-19 14:30:39 +0800

    fix: 默认添加model2Props

--- src/utils/writeBuildEntry.js
+++ src/utils/writeBuildEntry.js
@@ -4,7 +4,7 @@ const fse = require('fs-extra')
 const { formatPath, getDir } = require('./index')
 const writeBuildEntry = () => {
   const manifestJson = getManifestJson()
-  const { buildImport = {}, excludeNidAndUiType, useManifest = true } = getLibraConfig()
+  const { buildImport = {}, excludeNidAndUiType = true, useManifest = true, useModel2Props = true } = getLibraConfig()
   let imp = ''
   let impLess = ''
   let expStr = ''
@@ -32,7 +32,8 @@ const writeBuildEntry = () => {
         // const _manifestExists = fse.existsSync(`${_path}/manifest.tsx`) || fse.existsSync(`${_path}/manifest.ts`) || fse.existsSync(`${_path}/manifest.jsx`) || fse.existsSync(`${_path}/manifest.js`)
         if (useManifest) {
           if (_manifestExists) {
-            imp += `import ${item}Comp from '${_path}'\nimport ${item}Manifest from '${_path}/manifest'\nconst ${item} = ReactWrapper(${item}Comp, ${item}Manifest${excludeNidAndUiType ? ', { excludeNidAndUiType: true }' : ''})\n`
+            imp += `import ${item}Comp from '${_path}';\nimport ${item}Manifest from '${_path}/manifest';\nconst ${item} = ReactWrapper(${item}Comp, ${item}Manifest${excludeNidAndUiType ? ', { excludeNidAndUiType: true }' : ''});\n`
+            useModel2Props && (imp += `${item}.model2Props = ${item}Comp.model2Props || undefined;\n`)
           } else {
             imp += `import ${item} from '${_path}'\n`
             console.log(`Component ${item} misses manifest file.`)
