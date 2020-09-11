43a2397724919aac19bdf1e1e25bb312e6b1c973
izbz wh <731215820@qq.com>
2020-09-11 11:31:03 +0800

    chore: format

--- src/utils/writeManifest.js
+++ src/utils/writeManifest.js
@@ -2,6 +2,7 @@ const { getManifestJson, getLibraConfig } = require('./index')
 const path = require('path')
 const fse = require('fs-extra')
 const chalk = require('chalk')
+const { paramCase, pascalCase } = require('change-case')
 function customJsonStringify (key, value) {
   if (typeof value === 'function') {
     return `/Function(${value.toString()})/`
@@ -10,31 +11,64 @@ function customJsonStringify (key, value) {
 }
 
 const writeManifest = () => {
-  const output = getLibraConfig().output
-  require('ignore-styles').default(['.sass', '.scss', '.png', '.jpg', '.jpeg', '.gif', '.css', '.less', '.svg'])
-  const jsdom = require('jsdom');
-  const { JSDOM } = jsdom;
-  const { window } = new JSDOM('...');
-  const { document } = (new JSDOM('...')).window;
-  global.window = window;
-  global.document = document;
-  global.navigator = global.window.navigator
-  const library = require(path.resolve(output.dist, 'index.js'))
+  const libConfig = getLibraConfig()
+  const output = libConfig.output
   const manifestComponents = []
-  Object.keys(library).forEach(compName => {
-    const Comp = library[compName]
-    // console.log(object)
-    if (typeof Comp !== 'function') {
-      console.warn(`${compName} is not a Class`)
-      // continue
-      return
-    }
-    const componentManifest = Comp.manifest
-    if (componentManifest) {
-      const outputManifest = { ...componentManifest, name: componentManifest.name.toLowerCase() }
-      manifestComponents.push(outputManifest)
-    }
-  })
+  const manifestJson = getManifestJson()
+  require('ignore-styles').default(['.sass', '.scss', '.png', '.jpg', '.jpeg', '.gif', '.css', '.less', '.svg'])
+  if (libConfig.device === 'PC') {
+    const components = manifestJson.components || {};
+    Reflect.ownKeys(components).forEach(item => {
+      let compManifest;
+      const componentName = pascalCase(item)
+      const cManifestPath = path.resolve(`components/${paramCase(componentName)}/manifest.js`)
+      compManifest = fse.readFileSync(cManifestPath, 'utf-8');
+      let type = compManifest.match(/TypeProps.[\w]+,/)
+      if (type) {
+        type = type[0].substring(10);
+        type = type.substring(0, type.length - 1);
+      } else {
+        type = 'control'
+      }
+      compManifest = compManifest.replace(/import[\s\S]*'yonui-extension'/, '');
+      compManifest = compManifest.replace(/uiObject:[\s]*UIObject.[\w]*,/, '');
+      compManifest = compManifest.replace(/type: TypeProps.[\w]*,/, `type: '${type}',`);
+      compManifest = compManifest.replace(/export default/, 'module.exports =');
+      const tempMainfestPath = path.resolve(`components/${paramCase(componentName)}/tempmanifest.js`)
+      fse.writeFileSync(tempMainfestPath, compManifest)
+      const compManifestJson = require(tempMainfestPath);
+      // console.log(compManifestJson)
+      if (compManifestJson) {
+        const outputManifest = { ...compManifestJson, name: compManifestJson.name.toLowerCase() }
+        manifestComponents.push(outputManifest)
+      }
+      fse.removeSync(tempMainfestPath)
+    })
+  } else {
+    const jsdom = require('jsdom');
+    const { JSDOM } = jsdom;
+    const { window } = new JSDOM('...');
+    const { document } = (new JSDOM('...')).window;
+    global.window = window;
+    global.document = document;
+    global.navigator = global.window.navigator
+    const library = require(path.resolve(output.dist, 'index.js'))
+    Object.keys(library).forEach(compName => {
+      const Comp = library[compName]
+      // console.log(object)
+      if (typeof Comp !== 'function') {
+        console.warn(`${compName} is not a Class`)
+        // continue
+        return
+      }
+      const componentManifest = Comp.manifest
+      if (componentManifest) {
+        const outputManifest = { ...componentManifest, name: componentManifest.name.toLowerCase() }
+        manifestComponents.push(outputManifest)
+      }
+    })
+  }
+
   // for (const compName in library) {
   //   if( )
   //   console.log(compName)
@@ -47,7 +81,6 @@ const writeManifest = () => {
   //   componentManifest && manifestComponents.push(componentManifest)
   // }
   // console.log(object)
-  const manifestJson = getManifestJson()
   // const manifestComponents = []
   // const loop = (obj, prefix = '') => {
   //   for (const key in obj) {
